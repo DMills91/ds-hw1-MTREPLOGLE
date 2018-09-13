e0b0edd273aeb84e86129f19090e846dab78bc91
MTREPLOGLE <42595044+MTREPLOGLE@users.noreply.github.com>
2018-09-13 17:52:23 -0400

    Connected Randomuser to Api

--- Public/JS/dashboard.app.js
+++ Public/JS/dashboard.app.js
@@ -1,26 +1,37 @@
-var dashboardApp = new Vue({
-el: '#dashboard',
+var fetchRandomUser = new Vue({
+el: '#randomUser',
 data: {
-task: [
-{
-name: 'Mike',
-email: '',
-birthday: '',
-Age: '',
-Country: '',
+    person: {
+      name: '',
+      location: '',
+      email:'',
+      dob:'',
+      picture:''
+  }
 },
-]
+computed:{
 
+},
 
+methods: {
+  fetchRandomUser () {
+    fetch('https://randomuser.me/api/')
+    .then( (response) => response.json())
+    .then( json =>{
+      fetchRandomUser.person.name = json.results[0].name.first +" "+ json.results[0].name.last;
+        fetchRandomUser.person.location = json.results[0].location.city;
+          fetchRandomUser.person.email = json.results[0].email;
+            fetchRandomUser.person.dob = json.results[0].dob.date;
+              fetchRandomUser.person.picture = json.results[0].picture.large;
+    })
+    .catch(err=> {
+      console.log('TASK FETCH ERROR:');
+      console.log(err);
+    })
+  }
+},
 
+created ()  {
+  this.fetchRandomUser();
 }
-
-
-
-
-
-
-
-}
-
-);
+})
