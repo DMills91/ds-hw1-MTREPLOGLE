7c96cc64aa0a49b2cb9d700894223a7474d3f1a5
MTREPLOGLE <42595044+MTREPLOGLE@users.noreply.github.com>
2018-09-14 18:16:07 -0400

    Create Age Method

--- Public/JS/dashboard.app.js
+++ Public/JS/dashboard.app.js
@@ -1,57 +1,54 @@
 var fetchRandomUser = new Vue({
-el: '#randomUser',
-data: {
+  el: '#randomUser',
+  data: {
     person: {
       name: '',
       location: '',
       email:'',
       dob:'',
       picture:''
-  }
-},
-computed:{
-  days_left: function () {
-        return moment(this.project.target_date).diff(moment(), 'days')
-},
-shorten_date: function (){
-  return dob.trunc(15);
-},
+    }
+  },
+  methods: {
+      getAge: function (dateString) {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+          age--;
+        }
+        return age;
+      },
+    pretty_date: function(d)
+    {
+      return moment(d).format("MMM Do YY");
+    },
 
-refresh_page: function (){
-  return refreshPage();{
-    window.location.reload();
-  }
-
-}
-
-
-},
-methods: {
-pretty_date: function(d)
-{
-  return moment(d).format("MMM Do YY");
-},
-
-
-
-  fetchRandomUser () {
-    fetch('https://randomuser.me/api/')
-    .then( (response) => response.json())
-    .then( json =>{
-      fetchRandomUser.person.name = json.results[0].name.first +" "+ json.results[0].name.last;
+    fetchRandomUser: function () {
+      fetch('https://randomuser.me/api/')
+      .then( (response) => response.json())
+      .then( json =>{
+        fetchRandomUser.person.name = json.results[0].name.first +" "+ json.results[0].name.last;
         fetchRandomUser.person.location = json.results[0].location.city;
-          fetchRandomUser.person.email = json.results[0].email;
-            fetchRandomUser.person.dob = json.results[0].dob.date;
-              fetchRandomUser.person.picture = json.results[0].picture.large;
-    })
-    .catch(err=> {
-      console.log('TASK FETCH ERROR:');
-      console.log(err);
-    })
-  }
-},
+        fetchRandomUser.person.email = json.results[0].email;
+        fetchRandomUser.person.dob = json.results[0].dob.date;
+        fetchRandomUser.person.picture = json.results[0].picture.large;
+      })
+      .catch(err=> {
+        console.log('TASK FETCH ERROR:');
+        console.log(err);
+      })
+    },
 
-created ()  {
-  this.fetchRandomUser();
-}
+    getemail:  function(email){
+      return "mailto:"+ email;
+    },
+
+
+    created ()  {
+      this.fetchRandomUser()
+    }
+  }
+
 })
