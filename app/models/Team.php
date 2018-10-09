94a9e4b7b8f1f48173d55b7e0f225eb7bedf892c
MTREPLOGLE <42595044+MTREPLOGLE@users.noreply.github.com>
2018-10-09 19:46:55 -0400

    change work to team

---
+++ app/models/Team.php
@@ -0,0 +1,40 @@
+<?php
+
+class Team
+{
+  public $id;
+  public $name;
+  public $hourly_rate;
+
+  public function  __construct($data) {
+    $this->id = intval($data['id']);
+    $this->comment = ($data['comment']);
+
+  }
+
+
+
+  public static function fetchAll() {
+      // 1. Connect to the database
+      $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+      // 2. Prepare the query
+      $sql = 'SELECT * FROM Teams';
+      $statement = $db->prepare($sql);
+      // 3. Run the query
+      $success = $statement->execute();
+      // 4. Handle the results
+      $arr = [];
+      while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        // 4.a. For each row, make a new work object
+        $theTeam =  new Team($row);
+        array_push($arr, $theTeam);
+      }
+      return $arr;
+    }
+
+
+
+
+
+
+}
