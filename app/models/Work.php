86943a98c1b436c91185e0909cc2c7a45acb3e4d
MTREPLOGLE <42595044+MTREPLOGLE@users.noreply.github.com>
2018-10-09 19:13:56 -0400

    update

--- app/models/Work.php
+++ app/models/Work.php
@@ -11,22 +11,7 @@
       $this->comment = $row['comment'];
 
       public static function getWorkByTaskId(int $taskId) {
-      // 1. Connect to the database
-      $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-      // 2. Prepare the query
-      $sql = 'SELECT * FROM Work WHERE task_id = ?';
-      $statement = $db->prepare($sql);
-      // 3. Run the query
-      $success = $statement->execute(
-          [$taskId]
-      );
-      // 4. Handle the results
-      $arr = [];
-      while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        // 4.a. For each row, make a new work object
-        $workItem =  new Work($row);
-        array_push($arr, $workItem);
-      }
+
       return $arr;
     }
 
