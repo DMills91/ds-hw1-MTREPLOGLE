da5e97370f810f0b2f37a0e2c4d9e22e26817be0
MTREPLOGLE <42595044+MTREPLOGLE@users.noreply.github.com>
2018-10-09 22:06:31 -0400

    create code

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -11,38 +11,37 @@ class Comment
   }
 
   public static function fetchAll() {
-      // 1. Connect to the database
-      $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-      // 2. Prepare the query
-      $sql = 'SELECT * FROM Teams';
-      $statement = $db->prepare($sql);
-      // 3. Run the query
-      $success = $statement->execute();
-      // 4. Handle the results
-      $arr = [];
-      while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        // 4.a. For each row, make a new work object
-        $theComment =  new Comment($row);
-        array_push($arr, $theComment);
-      }
-      return $arr;
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Teams';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute();
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      // 4.a. For each row, make a new work object
+      $theComment =  new Comment($row);
+      array_push($arr, $theComment);
     }
+    return $arr;
+  }
 
-    public function create()  {
-$db = new PDO(DB_SERVER, DB_USER, DB_PW);
-$sql = 'INSERT Teams (id, comment)
-        VALUES (?, ?)';
+  public function create()  {
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    $sql = 'INSERT INTO Teams (comment)
+    VALUES (?)';
 
-      $statement = $db->prepare($sql);
-      $success = $statement->execute([
-        $this->id,
-        $this->comment
-      ]);
+    $statement = $db->prepare($sql);
+    $success = $statement->execute([
+      $this->comment
+    ]);
 
-$this->id = $db->lastInsertId();
+    $this->id = $db->lastInsertId();
 
 
-    }
+  }
 
 
 
