d0e6199b957b639a191b56d9c95543d5914a3ec7
MTREPLOGLE <42595044+MTREPLOGLE@users.noreply.github.com>
2018-10-09 20:11:40 -0400

    change fetch to include comment

--- Public/api/comment.php
+++ Public/api/comment.php
@@ -6,9 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 // 1. Go to the database and get all teams
-$teams = Team::fetchAll();
+$comments = Comment::fetchAll();
 // 2. Convert to JSON
-$json = json_encode($teams, JSON_PRETTY_PRINT);
+$json = json_encode($comments, JSON_PRETTY_PRINT);
 // 3. Print
 header('Content-Type: application/json');
 echo $json;
