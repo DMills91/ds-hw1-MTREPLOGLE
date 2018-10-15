8f05fd38e095ac6624d929e5cb39736beb0068a5
MTREPLOGLE <42595044+MTREPLOGLE@users.noreply.github.com>
2018-10-15 19:49:05 -0400

    add Comment to table and post to table to api

--- Public/JS/comment.app.js
+++ Public/JS/comment.app.js
@@ -2,12 +2,53 @@ var fetchComment = new Vue({
   el: '#randomComment',
   data: {
     comment: {
-      id:'',
+      id:'0',
       comment:''
     },
   },
 
 methods: {
+
+
+  handleWorkForm() {
+
+    // TODO: Check validity in a better way
+    if (this.newcomment <= 0) {
+      console.error('Cannot submit, invalid values');
+      return;
+    }
+
+
+    const s = JSON.stringify(this.comment);
+
+    console.log(s);
+
+    // POST to remote server
+    fetch('api/comment.php', {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+          "Content-Type": "application/json; charset=utf-8"
+      },
+      body: s // body data type must match "Content-Type" header
+    })
+    .then( response => response.json() )
+    .then( json => {this.comment.push(json)})
+    .catch( err => {
+      console.error('COMMENT POST ERROR:');
+      console.error(err);
+    })
+
+    // Reset workForm
+    this.workForm = this.getEmptyWorkForm();
+  },
+
+
+
+
+
+
+
+
   fetchComment () {
       fetch('.../api/comment.php')
       .then( response => response.json() )
