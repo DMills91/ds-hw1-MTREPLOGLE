bc707547932b681996b784bc960889584f9ce8a4
MTREPLOGLE <42595044+MTREPLOGLE@users.noreply.github.com>
2018-10-16 10:21:23 -0400

    add app as ID tag to comment page

--- Public/JS/comment.app.js
+++ Public/JS/comment.app.js
@@ -1,70 +1,63 @@
 var fetchComment = new Vue({
   el: '#randomComment',
   data: {
-    comment: {
+    comment: [],
+    commentForm: {
       id:'0',
       comment:''
     },
   },
 
-methods: {
+  methods: {
+    newCommentTable() {
 
+      // TODO: Check validity in a better way
+      if (this.comment <= 0) {
+        console.error('Cannot submit, invalid values');
+        return;
+      }
 
-  newCommentTable() {
+      const s = JSON.stringify(this.comment);
 
-    // TODO: Check validity in a better way
-    if (this.comment <= 0) {
-      console.error('Cannot submit, invalid values');
-      return;
-    }
+      console.log(s);
 
-
-    const s = JSON.stringify(this.comment);
-
-    console.log(s);
-
-    // POST to remote server
-    fetch('api/comment.php', {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      headers: {
+      // POST to remote server
+      fetch('api/comment.php', {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        headers: {
           "Content-Type": "application/json; charset=utf-8"
-      },
-      body: s // body data type must match "Content-Type" header
-    })
-    .then( response => response.json() )
-    .then( json => {this.comment.push(json)})
-    .catch( err => {
-      console.error('COMMENT POST ERROR:');
-      console.error(err);
-    })
+        },
+        body: s // body data type must match "Content-Type" header
+      })
+      .then( response => response.json() )
+      .then( json => {fetchComment.comment.push(json)})
+      .catch( err => {
+        console.error('COMMENT POST ERROR:');
+        console.error(err);
+      })
 
-    // Reset workForm
-    this.workForm = this.getEmptyWorkForm();
-  },
-getEmptyWorkForm()  {
-return {
-comment:null
-
-}
-},
-fetchComment () {
+      // Reset workForm
+      this.workForm = this.getEmptyWorkForm();
+    },
+    getEmptyWorkForm()  {
+      return {
+        comment:null
+      }
+    },
+    fetchComment () {
       fetch('.../api/comment.php')
       .then( response => response.json() )
       .then( json => {
-        commentapp.comment = json;
-
+        fetchComment.comment = json;
       })
-    .catch( err => {
-      console.log('COMMENT FETCH ERROR:');
-      console.log(err);
-    })
-  },
+      .catch( err => {
+        console.log('COMMENT FETCH ERROR:');
+        console.log(err);
+      })
+    },
 
-  created ()  {
-    this.fetchComment()
-  },
-
-}
-
-
+    created ()  {
+      this.fetchComment()
+    },
+  }
 })
